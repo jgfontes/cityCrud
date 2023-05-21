947924ea36bc7efda877fb97e983183e1b4bad9d
Julio Conta Pessoal <ju.1996@outlook.com>
2023-05-21 10:45:02 -0300

    Initialize project

---
+++ src/test/java/com/jgfontes/cityCrud/CityCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgfontes.cityCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
