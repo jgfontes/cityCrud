04ec2894e061d093e81cb497775302efaadb0631
Julio Conta Pessoal <ju.1996@outlook.com>
2023-05-22 14:41:31 -0300

    Changed from Gradle to Maven

---
+++ src/main/java/com/jgfontes/CityCrud/CityCrudApplication.java
@@ -0,0 +1,13 @@
+package com.jgfontes.CityCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgfontes/CityCrud/CityCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgfontes.CityCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
